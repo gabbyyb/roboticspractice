439313262996d417c8cb642a38e5b230d40b3ad9
Gabby <821760@seq.org>
2024-09-15 23:20:19 +0000

    car class practice

--- Car.java
+++ Car.java
@@ -1,1 +1,25 @@
+public class Car {
+    int wheels;
+    String color;
+    int doors;
+    String name;
 
+    public Car(int carWheels, String carColor, int carDoors, String carName) {
+        wheels = carWheels;
+        color = carColor;
+        doors = carDoors;
+        name = carName;
+    }
+
+    public void printInfo() {
+        System.out.println("number of wheels: " + wheels);
+        System.out.println("car color: " + color);
+        System.out.println("number of doors: " + doors);
+        System.out.println("name of car" + name);
+    }
+
+    public static void main(String[] args) {
+        Car tesla = new Car(4, "purple", 10, "Mercedes Benz");
+        tesla.printInfo();
+    }
+}
